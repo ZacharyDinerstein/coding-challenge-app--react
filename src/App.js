6470a56ee8fae858c1527caffea2e8dae2e4a8c3
ZackDinerstein <zackdhome@gmail.com>
2019-06-14 18:19:42 +0300

    Created Card component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Card from './components/Card/Card';
 
-function App() {
-  return (
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        cards: [
+          {
+            question: "What's the difference between Null & Undefined?",
+            answers: [
+              {
+                answer: "Undefined is applied to a variable when it is not assigned any value.",
+                example: "var x;<br/>console.log(x); //Undefined"
+              },{
+                answer: "Null is a value that you assign to a variable that means 'Nothing'.",
+                example: "var x = null;<br />console.log(x); //null"
+              }
+            ]
+          }
+        ]
+      }
+    };
+  }
 
-    <div>
-      <h1>JAVASCRIPT</h1>
-      <Card />
-    </div >
-  );
+  render() {
+    return (
+      <div>
+        <h1>JAVASCRIPT</h1>
+        {this.state.data.cards.map((card) => {
+
+          console.log('card:', card)
+          return (
+            <Card cardData={card} />
+          )
+        })}
+      </div >
+    );
+  }
 }
 
-export default App;
-
 {/* <div className="card__answer-wrapper">
-  <p className="card__answer">Null is a value that you assign to a variable that means "Nothing".</p>
+  <p className="card__answer"></p>
   <div className="card__example-wrapper">
     <p className="card__example-title">For example:</p>
     <code className="card__example">
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,22 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Card extends Component {
   render() {
+    let card = this.props.cardData;
+
     return (
       <div className="card card--question">
-        <h3 className="card__question">What's the difference between Null & Undefined?</h3>
-        <div className="card__answer-wrapper">
-          <p className="card__answer">Undefined is applied to a variable when it is not assigned any value.</p>
-          <div className="card__example-wrapper">
-            <p className="card__example-title">For example:</p>
-              <pre>
-            <code className="card__example">
-                var x;
-                console.log(x); //Undefined
-            </code>
-              </pre>
-          </div>
-        </div>
+        <h3 className="card__question">{card.question}</h3>
+
+        {card.answers.map((answer) => {
+          return (
+            <div className="card__answer-wrapper">
+              <p className="card__answer">{answer.answer}</p>
+              <div className="card__example-wrapper">
+                {/* <p className="card__example-title">For example:</p> */}
+                <code className="card__example">
+                  <pre>
+                    {answer.example}
+                  </pre>
+                </code>
+              </div>
+            </div>
+          )
+        })};
       </div>
     )
   }
