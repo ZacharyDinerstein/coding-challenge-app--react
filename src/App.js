7d33c845a0ea5654531533450451b280f0fb7d45
ZackDinerstein <zackdhome@gmail.com>
2019-06-14 17:37:23 +0300

    initial commmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <div>
+      <h1>JAVASCRIPT</h1>
+      <div className="card card--question">
+        <h3 className="card__question">What's the difference between Null & Undefined?</h3>
+        <div className="card__answer-wrapper">
+          <p className="card__answer">Undefined is applied to a variable when it is not assigned any value.</p>
+          <div className="card__example-wrapper">
+            <p className="card__example-title">For example:</p>
+            <code className="card__example">
+              <pre>
+                var x;
+                console.log(x); //Undefined
+              </pre>
+            </code>
+          </div>
+        </div>
+        {/* <div className="card__answer-wrapper">
+          <p className="card__answer">Null is a value that you assign to a variable that means "Nothing".</p>
+          <div className="card__example-wrapper">
+            <p className="card__example-title">For example:</p>
+            <code className="card__example">
+              <pre>
+                var x = null;
+                console.log(x); //null
+            </pre>
+            </code>
+          </div>
+        </div> */}
+      </div >
+    </div >
   );
 }
 
