e60aaff307059a1d9f735fbc61ecddc7e4b3d571
ZackDinerstein <zackdhome@gmail.com>
2019-06-19 08:56:11 -0400

    Added links to the cards

--- src/components/Card/CardMainContent.js
+++ src/components/Card/CardMainContent.js
@@ -3,31 +3,35 @@ import React, { Component } from 'react';
 export default class CardMainContent extends Component {
 
    render() {
-      const { cardData } = this.props;
+      let { cardData } = this.props;
       console.log(cardData);
 
       return (
+         <>
+            {cardData.answers.map((answer, index) => {
+               return (
+                  <div key={index} className="card__answer-wrapper">
+                     <p className="card__answer">{answer.answer}</p>
 
-         cardData.answers.map((answer, index) => {
-            return (
-               <div key={index} className="card__answer-wrapper">
-                  <p className="card__answer">{answer.answer}</p>
-
-                  {answer.example &&
-                     <div className="card__example-wrapper">
-                        <p className="card__example-title">Example:</p>
-                        <pre>
-                           <code className="card__example">
-                              {answer.example}
-                           </code>
-                        </pre>
-                     </div>
-                  }
-
-               </div>
-            )
-         })
-
+                     {answer.example &&
+                        <div className="card__example-wrapper">
+                           <p className="card__example-title">Example:</p>
+                           <pre>
+                              <code className="card__example">
+                                 {answer.example}
+                              </code>
+                           </pre>
+                        </div>
+                     }
+                  </div>
+               )
+            })}
+            {cardData.links.map((link, index) => {
+               return (
+                  <a href={link}>Link 1</a>
+               )
+            })}
+         </>
       )
    }
 }
