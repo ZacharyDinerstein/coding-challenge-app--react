c38215858cd2c65253923724f577e60f572abcbc
ZackDinerstein <zackdhome@gmail.com>
2019-06-20 08:04:40 -0400

    added indentation key

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -43,6 +43,20 @@ export default class Form extends Component {
     this.resetState();
   }
 
+  allowTabs = (e) => {
+    let t = e.target;
+    
+    if (e.keyCode === 91) {
+      let v = t.value,
+      s = t.selectionStart,
+      e = t.selectionEnd;
+      
+      t.value = v.substring(0, s) + '\t' + v.substring(e);
+      t.selectionStart = t.selectionEnd = s + 1;
+      return false;
+    }
+  }
+
 
 
   render() {
@@ -53,48 +67,50 @@ export default class Form extends Component {
           onSubmit={this.createCard}
           className="card card--form">
           <h1>Create New Card</h1>
+          <p className="sub-header">(Use COMMAND key to indent)</p>
           <div className="form__inputs-wrapper">
-              <label htmlFor="">Question</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Question Here"
-                name="question"
-                index={0}
-                onChange={this.updateCardAttribute}
-                value={this.state.question}
-              />
-              <label htmlFor="">Answer</label>
-              <textarea
-                type="text"
-                className="form-control"
-                placeholder="Answer Here"
-                name="answer"
-                onChange={this.updateCardAttribute}
-                value={this.state.answer}
-              />
-              
-              <label htmlFor="">Code Example</label>
-              <textarea
-                type="text"
-                className="form-control"
-                placeholder="Example Here"
-                name="example"
-                onChange={this.updateCardAttribute}
-                value={this.state.example}
-              />
-              <label htmlFor="">Link URL</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Link Here"
-                name="link"
-                onChange={this.updateCardAttribute}
-                value={this.state.link}
-              />
+            <label htmlFor="">Question</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Question Here"
+              name="question"
+              index={0}
+              onChange={this.updateCardAttribute}
+              value={this.state.question}
+            />
+            <label htmlFor="">Answer</label>
+            <textarea
+              type="text"
+              className="form-control"
+              placeholder="Answer Here"
+              name="answer"
+              onChange={this.updateCardAttribute}
+              value={this.state.answer}
+              onKeyDown={this.allowTabs}
+            />
+
+            <label htmlFor="">Code Example</label>
+            <textarea
+              type="text"
+              className="form-control"
+              placeholder="Example Here"
+              name="example"
+              onChange={this.updateCardAttribute}
+              value={this.state.example}
+            />
+            <label htmlFor="">Link URL</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Link Here"
+              name="link"
+              onChange={this.updateCardAttribute}
+              value={this.state.link}
+            />
           </div>
 
-          <button class="hidden" type="submit">Submit</button>
+          <button className="hidden" type="submit">Submit</button>
         </form>
         <div></div>
       </>
