3d6088eba8b78ac32659d60304ba7eadfd898d2f
ZackDinerstein <zackdhome@gmail.com>
2019-07-15 17:07:57 +0300

    Added hook to Card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,30 +1,23 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import CardMainContent from './CardMainContent';
 import Toggle from '../Helper/Toggle';
 import './Card.scss';
 
-export default class Card extends Component {
-  state = {
-    fullWidth: false
-  };
+const Card = (props) => {
 
-  toggleCardFullWidth = () => {
-    this.setState({
-      fullWidth: !this.state.fullWidth
-    })
-  }
+  const [fullWidth, toggleCardFullWidth] = useState(false);
+  
+  let { cardData } = props;
+  let classes = fullWidth ? "card card--question card--full-width" : "card card--question";
 
-  render() {
-    let { cardData } = this.props;
-    let classes = this.state.fullWidth ? "card card--question card--full-width" : "card card--question";
-
-    return (
-      <div className={classes} >
-        <h3 className="card__question">{cardData.question}</h3>
-        <Toggle toggleCardFullWidth={this.toggleCardFullWidth}>
-          <CardMainContent cardData={cardData} />
-        </Toggle>
-      </div>
-    )
-  }
+  return (
+    <div className={classes} >
+      <h3 className="card__question">{cardData.question}</h3>
+      <Toggle toggleCardFullWidth={() => toggleCardFullWidth(!fullWidth)}>
+        <CardMainContent cardData={cardData} />
+      </Toggle>
+    </div>
+  )
 }
+
+export default Card;
